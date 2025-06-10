3c1003941616d72fa5dae59b325e7f9c61ca369f
n2sy <nidhal.jelassi@fsegt.utm.tn>
2025-06-10 12:07:59 +0100

    add create todo

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -12,6 +12,11 @@ exports.getTodo = (req, res) => {
     res.status(404).json({ message: "Aucun Todo n'existe avec cet id" });
   res.status(202).json({ message: "Todo Trouvé", todo: selectedTodo });
 };
+exports.createTodo = (req, res) => {
+  let newTodo = req.body;
+  newTodo.id = crypto.randomUUID();
+  todoList.push(newTodo);
+  res.json({ message: "Todo ajouté avec succès", tab: todoList });
+};
 exports.updateTodo = (req, res) => {};
-exports.createTodo = (req, res) => {};
 exports.deleteTodo = (req, res) => {};
