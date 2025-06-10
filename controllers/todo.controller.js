0f3d320004f6a59ca03a7c31da0802d6461c64d7
n2sy <nidhal.jelassi@fsegt.utm.tn>
2025-06-10 11:43:43 +0100

    add getTodoById

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -6,7 +6,14 @@ exports.getAllTodos = (req, res) => {
     listeTodos: todoList,
   });
 };
-exports.getTodo = (req, res) => {};
+exports.getTodo = (req, res) => {
+  let selectedTodo = todoList.todoList.find(
+    (element) => element.id == req.params.id
+  );
+  if (!selectedTodo)
+    res.status(404).json({ message: "Aucun Todo n'existe avec cet id" });
+  res.status(202).json({ message: "Todo Trouvé", todo: selectedTodo });
+};
 exports.updateTodo = (req, res) => {};
 exports.createTodo = (req, res) => {};
 exports.deleteTodo = (req, res) => {};
