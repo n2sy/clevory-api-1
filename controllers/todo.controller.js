590793c5c1e1b8dd42869726a75fe390a8007679
n2sy <nidhal.jelassi@fsegt.utm.tn>
2025-06-10 11:17:15 +0100

    add controllers

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,4 +1,11 @@
-exports.getAllTodos = (req, res) => {};
+const todoList = require("../models/todo.model");
+
+exports.getAllTodos = (req, res) => {
+  res.json({
+    message: "List de Todos Récupérés",
+    listeTodos: todoList,
+  });
+};
 exports.getTodo = (req, res) => {};
 exports.updateTodo = (req, res) => {};
 exports.createTodo = (req, res) => {};
